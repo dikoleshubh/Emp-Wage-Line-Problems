b5600de279bae1ef35c4c43e8a27fc5d3b83483c
dikoleshubh <79848187+dikoleshubh@users.noreply.github.com>
2021-04-18 21:23:21 +0530

    COMPARISION OF LENGTH OF TWO LINES

--- Class1.cs
+++ Class1.cs
@@ -6,46 +6,7 @@ namespace ConsoleApp6
 {
     class Cartecian
     {
-        public static void CartecianR()
-        {
-            Console.WriteLine("Enter the Co Ordinates x1");
-            Console.ReadLine();
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Co Ordinates Y1");
-            Console.ReadLine();
-            int y = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Co Ordinates X2");
-            Console.ReadLine();
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Co Ordinates Y2");
-            Console.ReadLine();
-            int b = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the Co Ordinates x1");
-            Console.ReadLine();
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Co Ordinates Y1");
-            Console.ReadLine();
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Co Ordinates X2");
-            Console.ReadLine();
-            int a1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Co Ordinates Y2");
-            Console.ReadLine();
-            int b1 = Convert.ToInt32(Console.ReadLine());
-
-            int m = (a - x);
-            int n = (b - y);
-            double v = Convert.ToDouble(m);
-            double w = Convert.ToDouble(n);
-            double line1 = Math.Sqrt(v + w);
-            int m1 = (a1 - x1);
-            int n1 = (b1 - y1);
-            double v1 = Convert.ToDouble(m1);
-            double w1 = Convert.ToDouble(n1);
-            double line2 = Math.Sqrt(v1 + w1);
-
-            
+                 
 
             
             lineCompare(line1, line2); ////cOMPARISION OF TWO LINES
